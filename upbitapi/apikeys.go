eb877ae830c3d2044c57850ad429cb07e11f7306
raccoon-mh <wsx1341@gmail.com>
2024-11-14 17:55:07 +0000

    remove underbar

--- upbitapi/apikeys.go
+++ upbitapi/apikeys.go
@@ -11,6 +11,8 @@ type ApikeysGetResponse struct {
 
 type ApikeysGetResponses []ApikeysGetResponse
 
+// API 키 리스트 조회
+// https://docs.upbit.com/reference/open-api-%ED%82%A4-%EB%A6%AC%EC%8A%A4%ED%8A%B8-%EC%A1%B0%ED%9A%8C
 func ApikeysGet(ctx context.Context) (*ApikeysGetResponses, error) {
 	return commonAnyCaller(ctx, apiKeysEndPoint, RequestForm{}, &ApikeysGetResponses{})
 }
